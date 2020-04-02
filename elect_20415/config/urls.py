8c46af056c5d7ef01f950037473f9d9b12310235
Edwin <edwin.mucho@gmail.com>
2020-04-02 23:18:10 +0900

    add models

--- elect_20415/config/urls.py
+++ elect_20415/config/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('kakao/', include('kakao.urls')),
+    path('election/', include('crawl_elect.urls')),
 ]
--- elect_20415/crawl_elect/models.py
+++ elect_20415/crawl_elect/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Elect(models.Model):
+class Candidate(models.Model):
     ep = models.CharField(max_length=20) # 선거구
     pic = models.CharField(max_length=200) # 사진
     num = models.IntegerField() # 기호
@@ -9,19 +9,20 @@ class Elect(models.Model):
     name = models.CharField(max_length=30) # 이름
     gender = models.CharField(max_length=10) # 성별
     birth = models.CharField(max_length=30) # 생년월일 (연령)
-    adress = models.CharField(max_length=30) # 주소
+    address = models.CharField(max_length=30) # 주소
     job = models.CharField(max_length=30) # 직업
     level = models.CharField(max_length=50) # 학력
     career = models.CharField(max_length=100) # 경력
-    wealth = models.IntegerField() # 재산
+    wealth = models.CharField(max_length=50) # 재산
     military = models.CharField(max_length=50) # 병역
-    tax_total = models.IntegerField() # 납부액
-    tax_5y = models.IntegerField() # 최근 5년 체납
-    tax_defalt = models.IntegerField() # 현 체납액
-    crim_cnt = models.IntegerField() # 전과 횟수
-    candi_cnt = models.IntegerField() # 입후보 횟수
+    tax_total = models.CharField(max_length=50) # 납부액
+    tax_5y = models.CharField(max_length=50) # 최근 5년 체납
+    tax_defalt = models.CharField(max_length=50) # 현 체납액
+    crim_cnt = models.CharField(max_length=50) # 전과 횟수
+    candi_cnt = models.CharField(max_length=50) # 입후보 횟수
 
-
+    def __str__(self):
+        return self.name
 
 class Precinct(models.Model):
     election = models.CharField(max_length=20) # 선거구
