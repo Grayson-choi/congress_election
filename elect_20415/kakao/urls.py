542e8123f492ce4990ea9a47a39079eb96daa5c6
Grayson-choi <784wldnd@gmail.com>
2020-04-05 15:47:37 +0900

    AWS beanstalk 에업로드 할 준비

--- elect_20415/kakao/urls.py
+++ elect_20415/kakao/urls.py
@@ -6,14 +6,9 @@ app_name = 'kakao'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('checksever/', views.checkserver, name='chkserver'),
     path('message/', views.message, name='message'),
-    path('context/', views.context),
-    path('context2/', views.context2),
-    path('context3/', views.context3),
-    path('message/', views.message, name='message') ,
+    path('sgg/', views.sgg, name='send_url'),
     path('filter/<str:sgg>/', views.filter_candidates, name='filter'),
-    path('send_url/', views.send_url, name='send_url'),
     path('name/', views.name, name="name"),
     path('searchname/<str:name>/', views.search_name, name="search_name"),
     path('juso/', views.juso, name='juso'),
--- elect_20415/kakao/views.py
+++ elect_20415/kakao/views.py
@@ -9,13 +9,9 @@ from django.db import models
 
 from crawl_elect.models import Candidate, Precinct
 
-
-
-
-
 # Create your views here.
 def index(request):
-
+    #전체 후보자 조
     candidates = Candidate.objects.all()
 
     for candidate in candidates:
@@ -30,19 +26,7 @@ def index(request):
         'candidates': candidates
     }
 
-    return render(request, 'kakao/index.html', context)
-
-def search_sgg(request, dong):
-    all_sgg = Precinct.objects.all()
-
-    sggs = Precinct.objects.filter(dong=dong)
-
-    sgg_list = []
-
-    context = {
-        'sggs': all_sgg
-    }
-    return render(request, 'kakao/search_sgg.html', context)
+    return render(request, 'kakao/filter.html', context)
 
 
 def filter_candidates(request, sgg):
@@ -64,69 +48,13 @@ def filter_candidates(request, sgg):
     return render(request, 'kakao/filter.html', context)
 
 
-def name_candidates(request, name):
-    # 이름으로 후보자 조회
-    candidates = Candidate.objects.filter(name__contains=name)
-
-    for candidate in candidates:
-        if "아니한" in candidate.military:
-            candidate.military = "X"
-        elif "마친사람" in candidate.military:
-            candidate.military = "O"
-        else:
-            candidate.military = "-"
-
-    context = {
-        'candidates': candidates
-    }
-
-    return render(request, 'kakao/name.html', context)
-
 @csrf_exempt
-def filter_name(request):
+def sgg(request):
     answer = ((request.body).decode('utf-8'))
     return_json_str = json.loads(answer)
 
     return_str = return_json_str['action']['params']['sgg']
     print(return_json_str)
-    # print(return_str)
-    # print(return_json_str['action']['params']['sgg'])
-    return JsonResponse({
-        'version': "2.0",
-        'template': {
-            'outputs': [{
-                'simpleText': {
-                    'text': f"2020 국회의원 선거 후보자 정보입니다.\nhttps://cff01795.ngrok.io/kakao/name/{return_str}"
-                }
-            }],
-            'quickReplies': [
-                {'label': '전체 조회',
-                'action': 'message',
-                'messageText': '전체 후보자 조회'},
-                {'label': '선거구별 조회',
-                'action': 'message',
-                'messageText': '선거구별 조회'},
-                {'label': '이름으로 조회',
-                 'action': 'message',
-                 'messageText': '이름으로 조회'},
-            ]
-        }
-    })
-
-
-
-
-
-
-@csrf_exempt
-def send_url(request):
-    answer = ((request.body).decode('utf-8'))
-    return_json_str = json.loads(answer)
-
-    return_str = return_json_str['action']['params']['sgg']
-    print(return_json_str)
-    # print(return_str)
-    # print(return_json_str['action']['params']['sgg'])
     return JsonResponse({
         'version': "2.0",
         'template': {
@@ -145,19 +73,11 @@ def send_url(request):
                 {'label': '이름으로 조회',
                  'action': 'message',
                  'messageText': '이름으로 조회'},
+                {'label': '주소로찾기',
+                 'action': 'block',
+                 'blockId': '5e889e80b1fdff0001d6758c'}
             ]
         }
-    })
-
-
-
-
-
-
-# Create your views here.
-def keyboard(request):
-    return JsonResponse({
-        'type': 'text'
     })
 
 @csrf_exempt
@@ -174,123 +94,21 @@ def message(request):
                     'text': "2020 국회의원 선거 후보자 정보입니다.\nhttps://cff01795.ngrok.io/kakao/"
                 }
             }],
-            'quickReplies': [{
-                'label': '처음으로',
+            'quickReplies': [
+                {'label': '전체 조회',
                 'action': 'message',
-                'messageText': '처음으로'
-            }]
-        }
-    })
-
-
-SIM_MSG = 0
-SIM_IMG = 1
-SIM_CARD = 2
-
-BTN_MSG = 0
-BTN_LNK = 1
-
-
-@csrf_exempt
-def checkserver(request):
-    req = json.loads(request.body)
-
-    print('---' * 20)
-    pprint(req)
-    print('***' * 20)
-    img = "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg"
-    json_res = makeMessage(SIM_IMG, img, 'hello kakao', 1)
-
-    return JsonResponse(json_res, safe=False)
-
-
-def makeMessage(type, *args):
-    if type == SIM_MSG:
-        output = {
-            "simpleText": {
-                "text": f'{args[0]}'
-            }
-        }
-    elif type == SIM_IMG:
-        output = {
-            "simpleImage": {
-                "imageUrl": f'{args[0]}',
-                "altText": f'{args[1]}'
-            }
-        }
-    elif type == SIM_CARD:
-        output = makeCard()
-
-    message = {
-        "version": "2.0",
-        "template": {
-            "outputs": [
-                output
+                'messageText': '전체 후보자 조회'},
+                {'label': '선거구별 조회',
+                'action': 'message',
+                'messageText': '선거구별 조회'},
+                {'label': '이름으로 조회',
+                 'action': 'message',
+                 'messageText': '이름으로 조회'},
+                {'label': '주소로찾기',
+                 'action': 'block',
+                 'blockId': '5e889e80b1fdff0001d6758c'}
             ]
         }
-    }
-
-    return message
-
-
-def makeCard():
-    btns = makeButton()
-
-    result = {
-        "basicCard": {
-            "title": "보물상자",
-            "description": "보물상자 안에는 뭐가 있을까",
-            "thumbnail": {
-                "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg"
-            },
-
-            "buttons": [
-                {
-                    "action": "message",
-                    "label": "열어보기",
-                    "messageText": "짜잔! 우리가 찾던 보물입니다"
-                },
-                {
-                    "action": "webLink",
-                    "label": "구경하기",
-                    "webLinkUrl": "https://e.kakao.com/t/hello-ryan"
-                }
-            ],
-
-            "profile": {
-                "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4BJ9LU4Ikr_EvZLmijfcjzQKMRCJ2bO3A8SVKNuQ78zu2KOqM",
-                "nickname": "보물상자"
-            },
-
-            "social": {
-                "like": 1238,
-                "comment": 8,
-                "share": 780
-            }
-        }
-    }
-
-    return result
-
-
-def makeBtn(*args):
-    result = []
-
-    for i in range(num):
-        btn = {}
-        btn["action"] = args[i][0]
-        btn['label'] = args[i][1]
-        # ---
-        btn['webLinkUrl'] = args[i][2]
-        btn['messageText'] = args[i][2]
-
-    return result
-
-
-# Create your views here.
-def keyboard(request):
-    return JsonResponse({
-        'type': 'text'
     })
 
 @csrf_exempt
@@ -299,134 +117,32 @@ def message(request):
     return_json_str = json.loads(answer)
     return_str = return_json_str['userRequest']['utterance']
 
-    if return_str == '후보자':
-        return JsonResponse({
-            'version': "2.0",
-            'template': {
-                'outputs': [{
-                    'simpleText': {
-                        'text': "2020 국회의원 선거 후보자 정보입니다.\nhttps://0d328970.ngrok.io/kakao/"
-                    }
-                }],
-                'quickReplies': [{
-                    'label': '처음으로',
-                    'action': 'message',
-                    'messageText': '처음으로'
-                }]
-            }
-        })
-    else:
-        return JsonResponse({
-            'version': "2.0",
-            'template': {
-                'outputs': [{
-                    'simpleText': {
-                        'text': "2020 국회의원 선거 후보자 정보입니다.\nhttps://0d328970.ngrok.io/kakao/"
-                    }
-                }],
-                'quickReplies': [{
-                    'label': '처음으로',
-                    'action': 'message',
-                    'messageText': '처음으로'
-                }]
-            }
-        })
-
-from pprint import pprint
-
-@csrf_exempt
-def context(request):
-    answer = ((request.body).decode('utf-8'))
-    res = json.loads(answer)
-
-    pprint(res)
-
-    output ={
-        "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "simpleText": {
-                        "text": "컨텍스트 1"
-                    }
+    return JsonResponse({
+        'version': "2.0",
+        'template': {
+            'outputs': [{
+                'simpleText': {
+                    'text': "2020 국회의원 선거 후보자 정보입니다.\nhttps://cff01795.ngrok.io/kakao/"
                 }
+            }],
+            'quickReplies': [
+                {'label': '전체 조회',
+                 'action': 'message',
+                 'messageText': '전체 후보자 조회'},
+                {'label': '선거구별 조회',
+                 'action': 'message',
+                 'messageText': '선거구별 조회'},
+                {'label': '이름으로 조회',
+                 'action': 'message',
+                 'messageText': '이름으로 조회'},
+                {'label': '주소로찾기',
+                 'action': 'block',
+                 'blockId': '5e889e80b1fdff0001d6758c'}
             ]
         }
-    }
-    return JsonResponse(output)
+    })
 
 
-@csrf_exempt
-def context2(request):
-    answer = ((request.body).decode('utf-8'))
-    res = json.loads(answer)
-
-    pprint(res)
-
-    output ={
-        "version": "2.0",
-        "context": {
-            "values": [
-                {
-                    "name": "c_2",
-                    "lifeSpan": 10,
-                    "params": {
-                    "key2": "여기 컨텍스트 두번째 데이터 저장 되었습니다.",
-                    }
-                },
-            ]
-        },
-        "template": {
-            "outputs": [
-                {
-                    "simpleText": {
-                        "text": "컨텍스트 2"
-                    }
-                }
-            ]
-        }
-    }
-    return JsonResponse(output)
-
-@csrf_exempt
-def context3(request):
-    answer = ((request.body).decode('utf-8'))
-    res = json.loads(answer)
-
-    pprint(res)
-
-    # c_1 = res.get('contexts')[0]
-    # c_1 = c_1.get('params').get('key1').get("value") # 첫번째 블록에서 전달된 내용
-
-    # c_2 = res.get('contexts')[1]
-    # c_2 = c_2.get('params').get('key3').get("value") # 두번째 블록에서 전달된 내용
-    c_1= '1'
-    c_2='2'
-    output ={
-        "version": "2.0",
-        "context": {
-            "values": [
-                {
-                    "name": "c_1",
-                    "lifeSpan": 0,
-                },
-                {
-                    "name": "c_2",
-                    "lifeSpan": 0,
-                },
-            ]
-        },
-        "template": {
-            "outputs": [
-                {
-                    "simpleText": {
-                        "text": f"컨텍스트 3\n{c_1}\n{c_2}"
-                    }
-                }
-            ]
-        }
-    }
-    return JsonResponse(output)
 
 @csrf_exempt
 def name(request):
@@ -452,11 +168,25 @@ def name(request):
                         {
                             "action": "webLink",
                             "label": "후보자 확인",
-                            "webLinkUrl": f"https://05e30a2c.ngrok.io/kakao/searchname/{name}"
+                            "webLinkUrl": f"https://cff01795.ngrok.io/kakao/searchname/{name}"
                         }
                     ],
                 }
                 }
+            ],
+            'quickReplies': [
+                {'label': '전체 조회',
+                 'action': 'message',
+                 'messageText': '전체 후보자 조회'},
+                {'label': '선거구별 조회',
+                 'action': 'message',
+                 'messageText': '선거구별 조회'},
+                {'label': '이름으로 조회',
+                 'action': 'message',
+                 'messageText': '이름으로 조회'},
+                {'label': '주소로찾기',
+                 'action': 'block',
+                 'blockId': '5e889e80b1fdff0001d6758c'}
             ]
         }
     }
@@ -500,14 +230,32 @@ def juso(request):
                         {
                             "action": "webLink",
                             "label": "후보자 확인",
-                            "webLinkUrl": f"https://05e30a2c.ngrok.io/kakao/searchjuso/{juso}"
+                            "webLinkUrl": f"https://cff01795.ngrok.io/kakao/searchjuso/{juso}"
                         }
                     ],
                 }
                 }
+
+            ],
+            'quickReplies': [
+                {'label': '전체 조회',
+                 'action': 'message',
+                 'messageText': '전체 후보자 조회'},
+                {'label': '선거구별 조회',
+                 'action': 'message',
+                 'messageText': '선거구별 조회'},
+                {'label': '이름으로 조회',
+                 'action': 'message',
+                 'messageText': '이름으로 조회'},
+                {'label': '주소로찾기',
+                         'action': 'block',
+                'blockId': '5e889e80b1fdff0001d6758c'}
             ]
+
         }
     }
+
+
 
     return JsonResponse(output)
 
