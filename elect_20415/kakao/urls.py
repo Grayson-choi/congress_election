7101cb9c8db945c902e4af113aca90a9835d5552
Edwin <edwin.mucho@gmail.com>
2020-04-05 14:16:53 +0900

    주소찾기 이름 찾기 추가

--- elect_20415/kakao/urls.py
+++ elect_20415/kakao/urls.py
@@ -13,5 +13,9 @@ urlpatterns = [
     path('context3/', views.context3),
     path('message/', views.message, name='message') ,
     path('filter/<str:sgg>/', views.filter_candidates, name='filter'),
-    path('send_url/', views.send_url, name='send_url')
+    path('send_url/', views.send_url, name='send_url'),
+    path('name/', views.name, name="name"),
+    path('searchname/<str:name>/', views.search_name, name="search_name"),
+    path('juso/', views.juso, name='juso'),
+    path('searchjuso/<str:juso>/', views.search_juso, name="search_juso"),
 ]
